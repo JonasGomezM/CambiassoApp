104a4610db4f8114891ce40df7b58d683b843094
Yonathan Gomez <123jona97@gmail.com>
2022-11-06 18:49:47 -0500

    AppCambiasso

---
+++ src/Componentes/Formulario.js
@@ -0,0 +1,72 @@
+import React,{Component} from "react";
+
+class Formulario extends Component{
+    constructor(){
+        super();
+        this.state={
+            articulo:"",
+            descripcion:"",
+            valor:"Alto",
+            usuario:"",
+            cantidad:0,
+            imagen:""            	
+        }
+        this.guardar = this.guardar.bind(this);
+        this.cambio = this.cambio.bind(this);
+    }
+
+    cambio(e){
+        //console.log(e.target.name, e.target.value)
+        const {name,value}=e.target;
+        console.log(name,value);
+        this.setState({
+            [name]:value
+         })
+         console.log(this.state);
+    }
+
+    guardar(e){
+        e.preventDefault();
+        console.log(this.state);
+        
+        
+    }
+
+    render(){
+        return(
+            <div className="card w-50 m-auto bg-dark mt-4 mb-4 border-white">
+                <h1 className="m-auto">Nuevo Articulo</h1>
+                <form className="card-body">
+                    <div className="form-group">
+                        <input type="text" name="articulo" className="form-control" onChange={this.cambio} placeholder="Nombre Articulo"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="descripcion" className="form-control mt-3" placeholder="Descripción Articulo"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="usuario" className="form-control mt-3" placeholder="Usuario"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="number" name="cantidad" className="form-control mt-3" placeholder="Cantidad"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="imagen" className="form-control mt-3" placeholder="Imagen Tarjeta (carpeta recursos)"/>
+                    </div>
+                    <h5 className="text-white mt-4">Valor</h5>
+                    <div className="form-group">
+                        <select className="form-control mt-3" name="valor">
+                            <option>Bajo</option>
+                            <option>Medio</option>
+                            <option>Alto</option>
+                        </select>
+                    </div>
+                    <div className="form-group d-grid gap-2 col-4 mx-auto mt-3">
+                        <button className="btn btn-primary" onClick={this.guardar} type="submit">Crear</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Formulario;
